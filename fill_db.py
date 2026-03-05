2a0f6ceedc040545e122822c9e438edb28323156
Anna Buckel <abuckel0503@gmail.com>
2026-03-05 19:45:14 +0000

    still not work

--- fill_db.py
+++ fill_db.py
@@ -4,19 +4,17 @@ from models import Champion, engine, SQLModel
 
 def populate_db():
     """Populate the database with champion data from the CSV file."""
-    # Drop and recreate all tables with the new schema
-    SQLModel.metadata.drop_all(engine)
-    SQLModel.metadata.create_all(engine)
     
     with open('opgg_champions.csv', 'r') as file:
         reader = csv.DictReader(file)
         with Session(engine) as session:
             for row in reader:
+                winrate = (row['winrate'].rstrip('%'))
                 champion = Champion(
                     champion_name=row['champion_name'],
                     tier_number=int(row['tier_number']),
                     role=row['role'],
-                    winrate=float(row['winrate'].rstrip('%')),
+                    winrate=float(winrate),
                     pickrate=float(row['pickrate'].rstrip('%'))
                 )
                 session.add(champion)
