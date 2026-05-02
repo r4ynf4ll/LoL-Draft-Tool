38124bdcc2e9216270d1e353e7b30571f0ace3d2
Anna Buckel <abuckel0503@gmail.com>
2026-05-02 01:51:27 +0000

    counter fetchin endpoint

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from sqlmodel import Session, select
 
-from models import Champion, engine, init_db
+from models import Champion, engine, init_db, Counter
 
 app = FastAPI()
 
@@ -12,14 +12,14 @@ def startup() -> None:
 	init_db()
 
 @app.get("/champ_stats")
-def get_champstats(champion: str):
+def get_champstats(champion: str, role: str):
     with Session(engine) as session:
         statement = (
             select(Champion)
             .where(Champion.champion_name == champion)
-            .order_by(Champion.role)
+            .where(Champion.role == role)
         )
-        results = session.exec(statement).all()
+        results = session.exec(statement).first()
         if results:
             return [
                 {
@@ -32,5 +32,28 @@ def get_champstats(champion: str):
                 for result in results
             ]
         return {"error": "Champion not found"}
+    
+@app.get("/counters")
+def get_counters(champion: str, role: str):
+     with Session(engine) as session:
+          statement = (
+            select(Counter)
+            .where(Counter.champion_name == champion)
+            .where(Counter.role == role)
+            .order_by(Counter.rank)
+        )
+          results = session.exec(statement).all()
+          if results:
+               return [
+                    {
+                         "champion_name": result.champion_name,
+                         "role": result.role,
+                         "rank": result.rank,
+                         "counter_champion": result.counter_champion,
+                         "matchup_winrate": result.matchup_winrate,
+                    }
+                    for result in results
+               ]
+          return {"error": "No counters found"}
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
